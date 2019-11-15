83175939a4ee2efebd7574c7b498dc8c6af6c2d6
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-11-16 00:34:43 +0100

    Election_2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const Election = artifacts.require('Election');
+const Election_2 = artifacts.require('Election_2');
 
 module.exports = function(deployer) {
-  deployer.deploy(Election);
+  deployer.deploy(Election_2);
 };
