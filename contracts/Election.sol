8b2717a5d0afda17b005710d72a12d7b14e0257e
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-11-16 01:57:49 +0100

    Added frontEnd

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,41 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+
+// contract Election{
+//     string public candidate;
+
+//     constructor() public{
+//         candidate = "Ma9rounaa";
+//     }
+// }
+
+
 contract Election{
-    string public candidate;
+
+    //Candidate Model
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store candidates
+    mapping(uint => Candidate) public candidates;
+
+    //Number of candidates
+    uint public candidatesCount;
 
     constructor() public{
-        candidate = "Ma9rounaa";
+        addCandidate("Nabil");
+        addCandidate("Kais");
+    }
+
+    /*
+    * Internal functions are the same as private, except that it's also accessible to contracts
+    that inherit from this contract.
+    */
+    function addCandidate(string memory _name) internal {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 const Election = artifacts.require('Election');
-const Election_2 = artifacts.require('Election_2');
 
 module.exports = function(deployer) {
-  deployer.deploy(Election_2);
+  deployer.deploy(Election);
 };
